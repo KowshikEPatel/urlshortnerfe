1a7e07578c1dca3ad7982be46705f501a57ab06e
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-06-18 12:11:11 +0530

    user data access implementation

--- src/Components/UserData.js
+++ src/Components/UserData.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { Link } from 'react-router-dom'
+
+import { UserContext } from '../Contexts/UserContext'
 
 export default function UserData() {
+    const contextData = useContext(UserContext)
+
+    const handleClick = ()=>{
+        contextData.setLoggedUser({})
+        contextData.setIsLoggedIn(false)
+    }
+
     return (
-        <div>
-            
+        <div className='container'>
+            <div className='row'>
+                <Link to='/'  onClick={handleClick}>Logout</Link>
+                <hr></hr>
+            </div>
+            <div className='row'>
+                <div className='col-md-6'>First Name</div>
+                <div className='col-md-6'>{contextData.loggedUser['firstName']}</div>
+            </div>
+            <div className='row'>
+                <div className='col-md-6'>Last Name</div>
+                <div className='col-md-6'>{contextData.loggedUser['lastName']}</div>
+            </div>
         </div>
     )
 }
