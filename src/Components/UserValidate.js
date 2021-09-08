dcf6042579c95fb4f9d11044e76e4e1afd4d571a
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-09-08 08:30:26 +0530

    user validation logic changed

--- src/Components/UserValidate.js
+++ src/Components/UserValidate.js
@@ -1,10 +1,10 @@
-
+import {Login} from 'react-router-dom';
 
 export default function UserValidate(){
     
     return <div className="jumbotron">
     <h1 className="display-4">Thank you for signing up</h1>
-    <p className="lead">Activate your account by using the link sent to your email </p>
+    <p className="lead"><Link className="btn btn-light text-success">Login</Link> </p>
     <hr className="my-4"></hr>
     </div>
 }
